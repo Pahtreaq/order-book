3c1e884fd84dec99dbb26bb0294cad4c27349bf0
mewEdge, Inc <listings@advocates-connect.com>
2019-12-07 00:58:56 -0500

    test 1

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,25 @@
+function getUpdatedBook(existingBook, incomingOrder) {
+  let updatedBook = []
+  let correspondingOrders = existingBook.filter((item) => {
+    return item.type === incomingOrder.type
+
+  })
+  if (correspondingOrders.length === existingBook.length) {
+    updatedBook = existingBook.concat(incomingOrder)
+  }
+  return updatedBook
+}
+
 function reconcileOrder(existingBook, incomingOrder) {
-  throw new Error('Not Implemented Yet')
+  let updatedBook = []
+
+  if (!existingBook.length) {
+    updatedBook = existingBook.concat(incomingOrder)
+
+  } else {
+    updatedBook = getUpdatedBook(existingBook, incomingOrder)
+  }
+  return updatedBook
 }
 
 module.exports = reconcileOrder
