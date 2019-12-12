d2c74b6025358e10d9340aa59689547dec1ef333
mewEdge, Inc <listings@advocates-connect.com>
2019-12-11 19:17:13 -0500

    a whole lot of work in progress

--- orderBook.js
+++ orderBook.js
@@ -2,24 +2,51 @@ function getUpdatedBook(existingBook, incomingOrder) {
   let updatedBook = []
   let correspondingOrders = existingBook.filter((item) => {
     return item.type === incomingOrder.type
-
   })
+
   if (correspondingOrders.length === existingBook.length) {
     updatedBook = existingBook.concat(incomingOrder)
   }
   return updatedBook
 }
 
-function reconcileOrder(existingBook, incomingOrder) {
-  let updatedBook = []
+//function findMatchingOrder(existingBook, incomingOrder) {
+// return existingBook.filter(bookItem => (bookItem.type !== incomingOrder.type) && !((bookItem.price !== incomingOrder.price) && (bookItem.quantity !== incomingOrder.quantity)))
 
-  if (!existingBook.length) {
-    updatedBook = existingBook.concat(incomingOrder)
 
-  } else {
-    updatedBook = getUpdatedBook(existingBook, incomingOrder)
-  }
-  return updatedBook
+
+function filterMatchingOrder(existingBook, incomingOrder) {
+  return existingBook.filter(bookItem => (bookItem.type === incomingOrder.type) && (bookItem.quantity <= incomingOrder.quantity) && (bookItem.price === incomingOrder.price))
+
 }
 
-module.exports = reconcileOrder
+function priceDecider(existingPrice, incomingPrice) {
+  const priceIndex = existingPrice - incomingPrice
+  if (priceIndex >= 0) {
+    return true
+  }
+}
+
+function reconcileOrder(existingBook, incomingOrder) {
+  const filteredBook = findMatchingOrder(existingBook, incomingOrder)
+  if (filteredBook.length && priceDecider(filteredBook[0].price, incomingOrder.price)) {
+    let updatedBook = filterMatchingOrder(existingBook, incomingOrder)
+
+    const updatedOrder = {
+      type: ((filteredBook[0].quantity > incomingOrder.quantity) ? filteredBook[0].type : incomingOrder.type),
+      quantity: Math.abs(filteredBook[0].quantity - incomingOrder.quantity),
+      price: ((filteredBook[0].price > incomingOrder.price) ? filteredBook[0].price : incomingOrder.price)
+    }
+
+    if (updatedOrder.quantity === 0) {
+      return updatedBook
+    } else {
+      updatedBook = [updatedBook, updatedOrder]
+      return updatedBook
+
+    }
+
+  }
+
+  module.exports = reconcileOrder
+}
--- tests.js
+++ tests.js
@@ -1,229 +1,178 @@
 const {
   expect
 } = require('chai')
+const {
+  describe,
+  it
+} = require('mocha')
 const reconcileOrder = require('./orderBook')
+
 describe('Order Book', () => {
-      describe('reconcileOrder', () => {
-            it('adds an order to the book when the book is empty and thus cannot fulfill the order', () => {
-                  const existingBook = []
-                  const incomingOrder = {
-                    type: 'sell',
-                    quantity: 10,
-                    price: 6150
-                  }
-                  const updatedBook = reconcileOrder(existingBook, incomingOrder)
+  describe('reconcileOrder', () => {
+    it('adds an order to the book when the book is empty and thus cannot fulfill the order', () => {
+      const existingBook = []
+      const incomingOrder = {
+        type: 'sell',
+        quantity: 10,
+        price: 6150
+      }
 
-                  expect(updatedBook).to.deep.equal([{
-                    type: 'sell',
-                    quantity: 10,
-                    price: 6150
-                  }])
+      const updatedBook = reconcileOrder(existingBook, incomingOrder)
 
-                  it('adds an order to the book when the book has orders of only the corresponding type, (i.e. a sell with no buys)', () => {
-                      const existingBook = [{
-                        type: 'sell',
-                        quantity: 10,
-                        price: 6150
-                      }]
-                      const incomingOrder = [{
-                        type: 'sell',
-                        quantity: 10,
-                        price: 6000
-                      }]
-                    }
+      expect(updatedBook).to.deep.equal([{
+        type: 'sell',
+        quantity: 10,
+        price: 6150
+      }])
+    })
 
+    it('adds an order to the book when the book has orders of only the corresponding type, (i.e. a sell with no buys)', () => {
+      const existingBook = [{
+        type: 'sell',
+        quantity: 10,
+        price: 6150
+      }]
+      const incomingOrder = {
+        sell: 'sell',
+        quantity: 12,
+        price: 6000
+      }
+      const updatedBook = reconcileOrder(existingBook, incomingOrder)
 
-                    const updatedBook = reconcileOrder(existingBook, incomingOrder)
-                    expect(updatedBook).to.deep.equal([{
-                      type: 'sell',
-                      quantity: 10,
-                      price: 6150
-                    }, ])
-                  })
+      expect(updatedBook).to.deep.equal([{
+        type: 'sell',
+        quantity: 10,
+        price: 6150
+      }])
+    })
 
-                function reconcileOrder(existingBook, incomingOrder) {
-                  let updatedBook = []
+    it('adds an order to the book when the book has a corresponding order type but it does not match', () => {
+      const existingBook = [{
+        type: 'buy',
+        quantity: 10,
+        price: 6150
+      }]
+      const incomingOrder = {
+        type: 'sell',
+        quantity: 12,
+        price: 6000
+      }
 
-                  if (!existingBook.length) {
-                    updatedBook = existingBook.concat(incomingOrder)
-                  } else {
-                    updatedBook = getUpdatedBook(existingBook, incomingOrder)
-                  }
-                  return updatedBook
-                }
+      const updatedBook = reconcileOrder(existingBook, incomingOrder)
 
-                module.exports = reconcileOrder
+      expect(updatedBook).to.deep.equal([{
+          type: 'buy',
+          quantity: 10,
+          price: 6150
+        },
+        {
+          type: 'sell',
+          quantity: 12,
+          price: 6000
+        }
+      ])
+    })
 
-                const incomingOrder = {
-                  type: 'sell',
-                  quantity: 10,
-                  price: 6150
-                }
+    it('fulfills an order and removes the matching order when the book contains a matching order of the same quantity', () => {
+      const existingBook = [{
+        type: 'buy',
+        quantity: 10,
+        price: 6150
+      }]
+      const incomingOrder = {
+        type: 'sell',
+        quantity: 10,
+        price: 6150
+      }
+      const updatedBook = reconcileOrder(existingBook, incomingOrder)
 
-                const updatedBook = reconcileOrder(existingBook, incomingOrder)
+      expect(updatedBook).to.deep.equal([])
+    })
 
-                expect(updatedBook).to.deep.equal([{
-                  type: 'sell',
-                  quantity: 10,
-                  price: 6150
-                }])
+    it('fulfills an order and reduces the matching order when the book contains a matching order of a larger quantity', () => {
+      const existingBook = [{
+        type: 'buy',
+        quantity: 15,
+        price: 6150
+      }]
+      const incomingOrder = {
+        type: 'sell',
+        quantity: 10,
+        price: 6150
+      }
 
-                it('adds an order to the book when the book has orders of the corresponding type (i.e. a sell with no buys)', () => {
-                    const existingBook = [{
-                      type: 'sell',
-                      quantity: 10,
-                      price: 6150
-                    }]
-                    const incomingOrder = {
-                      type: 'sell',
-                      quantity: 12,
-                      price: 6000
-                    }
+      const updatedBook = reconcileOrder(existingBook, incomingOrder)
 
-                    const updatedBook = reconcileOrder(existingBook, incomingOrder)
+      expect(updatedBook).to.deep.equal([{
+        type: 'buy',
+        quantity: 5,
+        price: 6150
+      }])
+    })
 
-                    expect(updatedBook).to.deep.equal([])
-                    //add an if statement and make an equal to or not
-                    if (incomingOrder.type === existingBook[0].type) {
-                      existingBook.concat(incomingOrder)
-                    } else {
-                      if (incomingOrder.quantity === existingBook[0].quantity && incomingOrder.price === existingBook[0].price) {
+    it('partially fulfills an order, removes the matching order and adds the remainder of the order to the book when the book contains a matching order of a smaller quantity', () => {
+      const existingBook = [{
+        type: 'buy',
+        quantity: 10,
+        price: 6150
+      }]
+      const incomingOrder = {
+        type: 'sell',
+        quantity: 15,
+        price: 6150
+      }
 
-                      }
-                    }
+      const updatedBook = reconcileOrder(existingBook, incomingOrder)
 
-                    it('adds an order to the book when the book has a corresponding order type but it does not match', () => {
-                      const existingBook = [{
-                        type: 'buy',
-                        quantity: 10,
-                        price: 6150
-                      }]
-                      const incomingOrder = {
-                        type: 'sell',
-                        quantity: 12,
-                        price: 6000
-                      }
+      expect(updatedBook).to.deep.equal([{
+        type: 'sell',
+        quantity: 5,
+        price: 6150
+      }])
+    })
 
-                      const updatedBook = reconcileOrder(existingBook, incomingOrder)
+    it('Extra Credit: it fulfills a mismatched order when both parties benefit', () => {
+      const existingBook = [{
+        type: 'buy',
+        quantity: 15,
+        price: 6000
+      }]
+      const incomingOrder = {
+        type: 'sell',
+        quantity: 15,
+        price: 5900
+      }
 
-                      expect(updatedBook).to.deep.equal([{
-                          type: 'buy',
-                          quantity: 10,
-                          price: 6150
-                        },
-                        {
-                          type: 'sell',
-                          quantity: 12,
-                          price: 6000
-                        }
-                      ])
-                    })
+      const updatedBook = reconcileOrder(existingBook, incomingOrder)
 
-                    //if incoming order qty is equal to existing order qty, And if incoming order price us equal to existing order price, then remove order.
-                    if ('fulfills an order and removes the matching order when the book contains a matching order of the same quantity', () => {
-                        const existingBook = [{
-                          type: 'buy',
-                          quantity: 10,
-                          price: 6150
-                        }]
-                        const incomingOrder = {
-                          type: 'sell',
-                          quantity: 10,
-                          price: 6150
-                        }
-                        if (incomingOrder.type === existingBook[0].type) {
-                          existingBook.concat(incomingOrder)
-                        } else {
-                          if (incomingOrder.quantity === existingBook[0].quantity && incomingOrder.price === existingBook.price)
-                        }
-                      })
+      expect(updatedBook).to.deep.equal([])
+    })
 
-                      const updatedBook = reconcileOrder(existingBook, incomingOrder)
+    it('Extra Credit: it does not fulfill a mismatched order when it does not benefit both parties', () => {
+      const existingBook = [{
+        type: 'buy',
+        quantity: 15,
+        price: 5900
+      }]
+      const incomingOrder = {
+        type: 'sell',
+        quantity: 15,
+        price: 6000
+      }
 
-                    expect(updatedBook).to.deep.equal([])
+      const updatedBook = reconcileOrder(existingBook, incomingOrder)
 
-                    it('fulfills an order and reduces the matching order when the book contains a matching order of a larger quantity', () => {
-                      const existingBook = [{
-                        type: 'buy',
-                        quantity: 15,
-                        price: 6150
-                      }]
-                      const incomingOrder = {
-                        type: 'sell',
-                        quantity: 10,
-                        price: 6150
-                      }
-
-                      const updatedBook = reconcileOrder(existingBook, incomingOrder)
-
-                      expect(updatedBook).to.deep.equal([{
-                        type: 'buy',
-                        quantity: 5,
-                        price: 6150
-                      }])
-                    })
-
-                    it('partially fulfills an order, removes the matching order and adds the remainder of the order to the book when the book contains a matching order of a smaller quantity', () => {
-                      const existingBook = [{
-                        type: 'buy',
-                        quantity: 10,
-                        price: 6150
-                      }]
-                      const incomingOrder = {
-                        type: 'sell',
-                        quantity: 15,
-                        price: 6150
-                      }
-
-                      const updatedBook = reconcileOrder(existingBook, incomingOrder)
-
-                      expect(updatedBook).to.deep.equal([{
-                        type: 'sell',
-                        quantity: 5,
-                        price: 6150
-                      }])
-                    })
-
-                    it.skip('Extra Credit: it fulfills a mismatched order when both parties benefit', () => {
-                      const existingBook = [{
-                        type: 'buy',
-                        quantity: 15,
-                        price: 6000
-                      }]
-                      const incomingOrder = {
-                        type: 'sell',
-                        quantity: 15,
-                        price: 5900
-                      }
-
-                      const updatedBook = reconcileOrder(existingBook, incomingOrder)
-
-                      expect(updatedBook).to.deep.equal([])
-                    })
-
-                    it.skip('Extra Credit: it does not fulfill a mismatched order when it does not benefit both parties', () => {
-                      const existingBook = [{
-                        type: 'buy',
-                        quantity: 15,
-                        price: 5900
-                      }]
-                      const incomingOrder = {
-                        type: 'sell',
-                        quantity: 15,
-                        price: 6000
-                      }
-
-                      const updatedBook = reconcileOrder(existingBook, incomingOrder)
-
-                      expect(updatedBook).to.deep.equal([{
-                          type: 'buy',
-                          quantity: 15,
-                          price: 5900
-                        },
-                        {
-                          type: 'sell',
-                          quantity: 15,
-                          price: 6000
-                        },
-                      ])
-                    })
+      expect(updatedBook).to.deep.equal([{
+          type: 'buy',
+          quantity: 15,
+          price: 5900
+        },
+        {
+          type: 'sell',
+          quantity: 15,
+          price: 6000
+        },
+      ])
+    })
+  })
+})
